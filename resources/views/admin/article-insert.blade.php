8ca819ee99a62dccfb34e66a7e5fc61500985d5f
chenxiaolong <403228075@qq.com>
2018-01-08 13:31:12 +0800

    文章添加未完成

--- resources/views/admin/article-insert.blade.php
+++ resources/views/admin/article-insert.blade.php
@@ -13,41 +13,48 @@
         <div class="am-u-sm-12 am-u-md-4 am-u-md-push-8"></div>
         <div class="am-u-sm-12 am-u-md-8 am-u-md-pull-4">
 
-            <form class="am-form am-form-horizontal" data-am-validator>
+            <form class="am-form am-form-horizontal" method="post" action="{{ url('/admin/article-insert') }}" enctype="multipart/form-data" data-am-validator>
                 {{ csrf_field() }}
                 <fieldset>
                 <div class="am-form-group">
                     <label for="article-title" class="am-u-sm-3 am-form-label">标题：</label>
                     <div class="am-u-sm-9">
-                        <input type="text" id="article-name" placeholder="标题" required>
+                        <input type="text" id="article-title" name="article_name" placeholder="标题" required>
+                    </div>
+                </div>
+
+                <div class="am-form-group">
+                    <label for="article-picture" class="am-u-sm-3 am-form-label">封面图片：</label>
+                    <div class="am-u-sm-9">
+                        <input type="text" id="article-title" name="article_picture" placeholder="封面图片" required>
                     </div>
                 </div>
 
                 <div class="am-form-group">
                     <label for="article-contact" class="am-u-sm-3 am-form-label">联系人：</label>
                     <div class="am-u-sm-9">
-                        <input type="text" id="article-contact" value="周文彪" placeholder="联系人" required>
+                        <input type="text" id="article-contact" name="article_contact" value="周文彪" placeholder="联系人" required>
                     </div>
                 </div>
 
                 <div class="am-form-group">
                     <label for="article-phone" class="am-u-sm-3 am-form-label">电话</label>
                     <div class="am-u-sm-9">
-                        <input type="tel" id="article-phone" value="13800138000" placeholder="输入你的电话号码" required>
+                        <input type="tel" id="article-phone" name="article_tel" value="13800138000" placeholder="输入你的电话号码" required>
                     </div>
                 </div>
 
                 <div class="am-form-group">
                     <label for="article-QQ" class="am-u-sm-3 am-form-label">QQ</label>
                     <div class="am-u-sm-9">
-                        <input type="number" pattern="[0-9]*" id="article-QQ" value="13800138000" placeholder="输入你的QQ号码" required>
+                        <input type="number" pattern="[0-9]*" id="article-QQ" value="13800138000" name="article_qq" placeholder="输入你的QQ号码" required>
                     </div>
                 </div>
 
                 <div class="am-form-group">
                     <label for="article-type" class="am-u-sm-3 am-form-label">文章类型</label>
                     <div class="am-u-sm-9">
-                        <select id="rticle-type" required>
+                        <select id="rticle-type" name="article_type" required>
                             <option value="">-=请选择所属类型=-</option>
                             <option value="1">商业招商</option>
                             <option value="2">商业拓展</option>
@@ -60,7 +67,7 @@
                 <div class="am-form-group">
                     <label for="article-intro" class="am-u-sm-3 am-form-label">内容</label>
                     <div class="am-u-sm-9">
-                        <textarea rows="10" minlength="10" maxlength="100" id="article-intro" placeholder="输入内容"></textarea>
+                        <textarea name="article_content" rows="10" minlength="10" maxlength="100" id="article-intro" placeholder="输入内容"></textarea>
                     </div>
                 </div>
 
