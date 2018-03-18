a34f33bd01fb7fda79cab4dbc92d241f55d5b113
陈小龙 <13480731740@qq.com>
2018-03-18 22:08:38 +0800

    修改样式

--- resources/views/include/mobile/_sudoku-sc.blade.php
+++ resources/views/include/mobile/_sudoku-sc.blade.php
@@ -1,13 +1,13 @@
 <div class="home">
     <div class="hd">
         <h1 class="page_title">广商通商业共享</h1>
-        <p class="page_desc" style="color:red;">加入-共享商超网购-线上线下销售-更轻松！</p>
+        <p class="page_desc" style="color:red;">共享-商业-商品-服务-金融</p>
     </div>
     <div class="bd">
         <div class="weui_cells_title">共享商超</div>
         <div class="weui_grids">
             <a href="{{ url('/article-view?type_id=10') }}" class="weui_grid" style="padding: 5px;">
-                <div class="weui_grid_icon" style="width: 100%; height: 70px;">
+                <div class="weui_grid_icon" style="width: 100%; height: 80px;">
                     <img src="{{ asset('/assets/i/rou.jpg') }}" alt="">
                 </div>
                 <p class="weui_grid_label">
@@ -15,7 +15,7 @@
                 </p>
             </a>
             <a href="{{ url('/article-view?type_id=10') }}" class="weui_grid" style="padding: 5px;">
-                <div class="weui_grid_icon" style="width: 100%; height: 70px;">
+                <div class="weui_grid_icon" style="width: 100%; height: 80px;">
                     <img src="{{ asset('/assets/i/fs.jpg') }}" alt="">
                 </div>
                 <p class="weui_grid_label">
@@ -23,7 +23,7 @@
                 </p>
             </a>
             <a href="{{ url('/article-view?type_id=11') }}" class="weui_grid" style="padding: 5px;">
-                <div class="weui_grid_icon" style="width: 100%; height: 70px;">
+                <div class="weui_grid_icon" style="width: 100%; height: 80px;">
                     <img src="{{ asset('/assets/i/gc.jpg') }}" alt="">
                 </div>
                 <p class="weui_grid_label">
@@ -31,7 +31,7 @@
                 </p>
             </a>
             <a href="{{ url('/article-view?type_id=12') }}" class="weui_grid" style="padding: 5px;">
-                <div class="weui_grid_icon" style="width: 100%; height: 70px;">
+                <div class="weui_grid_icon" style="width: 100%; height: 80px;">
                     <img src="{{ asset('/assets/i/c.jpg') }}" alt="">
                 </div>
                 <p class="weui_grid_label">
@@ -39,7 +39,7 @@
                 </p>
             </a>
             <a href="{{ url('/article-view?type_id=13') }}" class="weui_grid" style="padding: 5px;">
-                <div class="weui_grid_icon" style="width: 100%; height: 70px;">
+                <div class="weui_grid_icon" style="width: 100%; height: 80px;">
                     <img src="{{ asset('/assets/i/m.jpg') }}" alt="">
                 </div>
                 <p class="weui_grid_label">
@@ -47,7 +47,7 @@
                 </p>
             </a>
             <a href="{{ url('/article-view?type_id=13') }}" class="weui_grid" style="padding: 5px;">
-                <div class="weui_grid_icon" style="width: 100%; height: 70px;">
+                <div class="weui_grid_icon" style="width: 100%; height: 80px;">
                     <img src="{{ asset('/assets/i/gst_logo.png') }}" alt="">
                 </div>
                 <p class="weui_grid_label">
@@ -55,6 +55,9 @@
                 </p>
             </a>
         </div>
+    </div>
+    <div class="hd">
+        <p class="page_desc" style="color:red;">加入-共享商超网购-线上线下销售-更轻松！</p>
     </div>
 </div>
 
--- resources/views/include/mobile/_sudoku.blade.php
+++ resources/views/include/mobile/_sudoku.blade.php
@@ -7,43 +7,43 @@
         <div class="weui_cells_title">共享商业</div>
         <div class="weui_grids">
             <a href="{{ url('/article-view?type_id=5') }}" class="weui_grid" style="padding: 5px;">
-                <div class="weui_grid_icon" style="width: 100%; height: 70px; background-color: #f3cb71; text-align: center;">
-                    <p style="padding-top: 24px; color: #000;">
+                <div class="weui_grid_icon" style="width: 100%; height: 80px; background-color: #f3cb71; text-align: center;">
+                    <p style="padding-top: 30px; color: #000;">
                         商场招商
                     </p>
                 </div>
             </a>
             <a href="{{ url('/article-view?type_id=6') }}" class="weui_grid" style="padding: 5px;">
-                <div class="weui_grid_icon" style="width: 100%; height: 70px; background-color: #98d08c; text-align: center;">
-                    <p style="padding-top: 24px; color: #fff;">
+                <div class="weui_grid_icon" style="width: 100%; height: 80px; background-color: #98d08c; text-align: center;">
+                    <p style="padding-top: 30px; color: #fff;">
                         商业招商
                     </p>
                 </div>
             </a>
             <a href="{{ url('/article-view?type_id=7') }}" class="weui_grid" style="padding: 5px;">
-                <div class="weui_grid_icon" style="width: 100%; height: 70px; background-color: #9bc9db; text-align: center;">
-                    <p style="padding-top: 24px; color: #000;">
+                <div class="weui_grid_icon" style="width: 100%; height: 80px; background-color: #9bc9db; text-align: center;">
+                    <p style="padding-top: 30px; color: #000;">
                         商场买卖
                     </p>
                 </div>
             </a>
             <a href="{{ url('/article-view?type_id=8') }}" class="weui_grid" style="padding: 5px;">
-                <div class="weui_grid_icon" style="width: 100%; height: 70px; background-color: #f15a24; text-align: center;">
-                    <p style="padding-top: 24px; color: #fff;">
+                <div class="weui_grid_icon" style="width: 100%; height: 80px; background-color: #f15a24; text-align: center;">
+                    <p style="padding-top: 30px; color: #fff;">
                         商业买卖
                     </p>
                 </div>
             </a>
             <a href="{{ url('/article-view?type_id=9') }}" class="weui_grid" style="padding: 5px;">
-                <div class="weui_grid_icon" style="width: 100%; height: 70px; background-color: #fcdda5; text-align: center;">
-                    <p style="padding-top: 24px; color: #000;">
+                <div class="weui_grid_icon" style="width: 100%; height: 80px; background-color: #fcdda5; text-align: center;">
+                    <p style="padding-top: 30px; color: #000;">
                         设备买卖
                     </p>
                 </div>
             </a>
             <a href="{{ url('/article-view?type_id=9') }}" class="weui_grid" style="padding: 5px;">
-                <div class="weui_grid_icon" style="width: 100%; height: 70px; background-color: #0071bc; text-align: center;">
-                    <p style="padding-top: 24px; color: #fff;">
+                <div class="weui_grid_icon" style="width: 100%; height: 80px; background-color: #0071bc; text-align: center;">
+                    <p style="padding-top: 30px; color: #fff;">
                         品牌拓展
                     </p>
                 </div>
@@ -53,7 +53,7 @@
         <div class="weui_cells_title">共享商超</div>
         <div class="weui_grids">
             <a href="{{ url('/article-view?type_id=10') }}" class="weui_grid" style="padding: 5px;">
-                <div class="weui_grid_icon" style="width: 100%; height: 70px;">
+                <div class="weui_grid_icon" style="width: 100%; height: 80px;">
                     <img src="{{ asset('/assets/i/rou.jpg') }}" alt="">
                 </div>
                 <p class="weui_grid_label">
@@ -61,7 +61,7 @@
                 </p>
             </a>
             <a href="{{ url('/article-view?type_id=10') }}" class="weui_grid" style="padding: 5px;">
-                <div class="weui_grid_icon" style="width: 100%; height: 70px;">
+                <div class="weui_grid_icon" style="width: 100%; height: 80px;">
                     <img src="{{ asset('/assets/i/fs.jpg') }}" alt="">
                 </div>
                 <p class="weui_grid_label">
@@ -69,7 +69,7 @@
                 </p>
             </a>
             <a href="{{ url('/article-view?type_id=11') }}" class="weui_grid" style="padding: 5px;">
-                <div class="weui_grid_icon" style="width: 100%; height: 70px;">
+                <div class="weui_grid_icon" style="width: 100%; height: 80px;">
                     <img src="{{ asset('/assets/i/gc.jpg') }}" alt="">
                 </div>
                 <p class="weui_grid_label">
@@ -77,7 +77,7 @@
                 </p>
             </a>
             <a href="{{ url('/article-view?type_id=12') }}" class="weui_grid" style="padding: 5px;">
-                <div class="weui_grid_icon" style="width: 100%; height: 70px;">
+                <div class="weui_grid_icon" style="width: 100%; height: 80px;">
                     <img src="{{ asset('/assets/i/c.jpg') }}" alt="">
                 </div>
                 <p class="weui_grid_label">
@@ -85,7 +85,7 @@
                 </p>
             </a>
             <a href="{{ url('/article-view?type_id=13') }}" class="weui_grid" style="padding: 5px;">
-                <div class="weui_grid_icon" style="width: 100%; height: 70px;">
+                <div class="weui_grid_icon" style="width: 100%; height: 80px;">
                     <img src="{{ asset('/assets/i/m.jpg') }}" alt="">
                 </div>
                 <p class="weui_grid_label">
@@ -93,7 +93,7 @@
                 </p>
             </a>
             <a href="{{ url('/article-view?type_id=13') }}" class="weui_grid" style="padding: 5px;">
-                <div class="weui_grid_icon" style="width: 100%; height: 70px;">
+                <div class="weui_grid_icon" style="width: 100%; height: 80px;">
                     <img src="{{ asset('/assets/i/gst_logo.png') }}" alt="">
                 </div>
                 <p class="weui_grid_label">
