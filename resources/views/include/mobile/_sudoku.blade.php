1b451e277ebe047c12b1a1cc2e4619d3772b4464
陈小龙 <13480731740@qq.com>
2018-03-18 11:16:18 +0800

    图片修改

--- resources/views/include/mobile/_sudoku.blade.php
+++ resources/views/include/mobile/_sudoku.blade.php
@@ -4,166 +4,103 @@
         <p class="page_desc" style="color:red;">共享-商业-商品-服务-金融</p>
     </div>
     <div class="bd">
-        <div class="weui_cells_title">商超共享</div>
+        <div class="weui_cells_title">共享商业</div>
         <div class="weui_grids">
-            <a href="{{ url('/article-view?type_id=5') }}" class="weui_grid">
-                <div class="weui_grid_icon">
-                    <img src="{{ asset('assets/i/jin.jpg') }}" alt="">
+            <a href="{{ url('/article-view?type_id=5') }}" class="weui_grid" style="padding: 5px;">
+                <div class="weui_grid_icon" style="width: 100%; height: 70px; background-color: #f3cb71; text-align: center;">
+                    <p style="padding-top: 24px; color: #000;">
+                        商场招商
+                    </p>
                 </div>
-                <p class="weui_grid_label">
-                   商场招商
-                </p>
             </a>
-            <a href="{{ url('/article-view?type_id=6') }}" class="weui_grid">
-                <div class="weui_grid_icon">
-                    <img src="{{ asset('assets/i/mu.jpg') }}" alt="">
+            <a href="{{ url('/article-view?type_id=6') }}" class="weui_grid" style="padding: 5px;">
+                <div class="weui_grid_icon" style="width: 100%; height: 70px; background-color: #98d08c; text-align: center;">
+                    <p style="padding-top: 24px; color: #fff;">
+                        商业招商
+                    </p>
                 </div>
-                <p class="weui_grid_label">
-                    商业招商
-                </p>
             </a>
-            <a href="{{ url('/article-view?type_id=7') }}" class="weui_grid">
-                <div class="weui_grid_icon">
-                    <img src="{{ asset('assets/i/shui.jpg') }}" alt="">
+            <a href="{{ url('/article-view?type_id=7') }}" class="weui_grid" style="padding: 5px;">
+                <div class="weui_grid_icon" style="width: 100%; height: 70px; background-color: #9bc9db; text-align: center;">
+                    <p style="padding-top: 24px; color: #000;">
+                        商场买卖
+                    </p>
                 </div>
-                <p class="weui_grid_label">
-                    商场买卖
-                </p>
             </a>
-            <a href="{{ url('/article-view?type_id=8') }}" class="weui_grid">
-                <div class="weui_grid_icon">
-                    <img src="{{ asset('assets/i/huo.jpg') }}" alt="">
+            <a href="{{ url('/article-view?type_id=8') }}" class="weui_grid" style="padding: 5px;">
+                <div class="weui_grid_icon" style="width: 100%; height: 70px; background-color: #f15a24; text-align: center;">
+                    <p style="padding-top: 24px; color: #fff;">
+                        商业买卖
+                    </p>
                 </div>
-                <p class="weui_grid_label">
-                    商业买卖
-                </p>
             </a>
-            <a href="{{ url('/article-view?type_id=9') }}" class="weui_grid">
-                <div class="weui_grid_icon">
-                    <img src="{{ asset('assets/i/tu.jpg') }}" alt="">
+            <a href="{{ url('/article-view?type_id=9') }}" class="weui_grid" style="padding: 5px;">
+                <div class="weui_grid_icon" style="width: 100%; height: 70px; background-color: #fcdda5; text-align: center;">
+                    <p style="padding-top: 24px; color: #000;">
+                        设备买卖
+                    </p>
                 </div>
-                <p class="weui_grid_label">
-                    设备买卖
-                </p>
             </a>
-            <a href="{{ url('/article-view?type_id=9') }}" class="weui_grid">
-                <div class="weui_grid_icon">
-                    <img src="{{ asset('assets/i/map.jpg') }}" alt="">
+            <a href="{{ url('/article-view?type_id=9') }}" class="weui_grid" style="padding: 5px;">
+                <div class="weui_grid_icon" style="width: 100%; height: 70px; background-color: #0071bc; text-align: center;">
+                    <p style="padding-top: 24px; color: #fff;">
+                        品牌拓展
+                    </p>
                 </div>
-                <p class="weui_grid_label">
-                    品牌拓展
-                </p>
             </a>
         </div>
 
         <div class="weui_cells_title">共享商超</div>
         <div class="weui_grids">
-            <a href="{{ url('/article-view?type_id=10') }}" class="weui_grid">
-                <div class="weui_grid_icon">
-                    <i class="iconfont icon-zhekou"></i>
+            <a href="{{ url('/article-view?type_id=10') }}" class="weui_grid" style="padding: 5px;">
+                <div class="weui_grid_icon" style="width: 100%; height: 70px;">
+                    <img src="{{ asset('/assets/i/rou.jpg') }}" alt="">
                 </div>
                 <p class="weui_grid_label">
                     共享超市
                 </p>
             </a>
-            <a href="{{ url('/article-view?type_id=10') }}" class="weui_grid">
-                <div class="weui_grid_icon">
-                    <i class="iconfont icon-zhekou"></i>
+            <a href="{{ url('/article-view?type_id=10') }}" class="weui_grid" style="padding: 5px;">
+                <div class="weui_grid_icon" style="width: 100%; height: 70px;">
+                    <img src="{{ asset('/assets/i/fs.jpg') }}" alt="">
                 </div>
                 <p class="weui_grid_label">
                     共享商品
                 </p>
             </a>
-            <a href="{{ url('/article-view?type_id=11') }}" class="weui_grid">
-                <div class="weui_grid_icon">
-                    <i class="iconfont icon-gouwuchetianjia"></i>
+            <a href="{{ url('/article-view?type_id=11') }}" class="weui_grid" style="padding: 5px;">
+                <div class="weui_grid_icon" style="width: 100%; height: 70px;">
+                    <img src="{{ asset('/assets/i/gc.jpg') }}" alt="">
                 </div>
                 <p class="weui_grid_label">
                     共享商城
                 </p>
             </a>
-            <a href="{{ url('/article-view?type_id=12') }}" class="weui_grid">
-                <div class="weui_grid_icon">
-                    <i class="iconfont icon-hezuoguanxi-xianxing"></i>
+            <a href="{{ url('/article-view?type_id=12') }}" class="weui_grid" style="padding: 5px;">
+                <div class="weui_grid_icon" style="width: 100%; height: 70px;">
+                    <img src="{{ asset('/assets/i/c.jpg') }}" alt="">
                 </div>
                 <p class="weui_grid_label">
                     共享商家
                 </p>
             </a>
-            <a href="{{ url('/article-view?type_id=13') }}" class="weui_grid">
-                <div class="weui_grid_icon">
-                    <i class="iconfont icon-kefu"></i>
+            <a href="{{ url('/article-view?type_id=13') }}" class="weui_grid" style="padding: 5px;">
+                <div class="weui_grid_icon" style="width: 100%; height: 70px;">
+                    <img src="{{ asset('/assets/i/m.jpg') }}" alt="">
                 </div>
                 <p class="weui_grid_label">
                     共享服务
                 </p>
             </a>
-            <a href="{{ url('/article-view?type_id=13') }}" class="weui_grid">
-                <div class="weui_grid_icon">
-                    <i class="iconfont icon-kefu"></i>
+            <a href="{{ url('/article-view?type_id=13') }}" class="weui_grid" style="padding: 5px;">
+                <div class="weui_grid_icon" style="width: 100%; height: 70px;">
+                    <img src="{{ asset('/assets/i/gst_logo.png') }}" alt="">
                 </div>
                 <p class="weui_grid_label">
                     共享金融
                 </p>
             </a>
         </div>
-
-        <div class="weui_cells_title">共享超市</div>
-        <div class="weui_grids">
-            <a href="{{ url('/article-view?type_id=14') }}" class="weui_grid">
-                <div class="weui_grid_icon">
-                    <i class="iconfont icon-dianpu"></i>
-                </div>
-                <p class="weui_grid_label">
-                    嘉湖总店
-                </p>
-            </a>
-            <a href="{{ url('/article-view?type_id=15') }}" class="weui_grid">
-                <div class="weui_grid_icon">
-                    <i class="iconfont icon-gouwu"></i>
-                </div>
-                <p class="weui_grid_label">
-                    波海蓝湾店
-                </p>
-            </a>
-        </div>
-
-        <div class="weui_cells_title">共享金融</div>
-        <div class="weui_grids">
-            <a href="{{ url('/article-view?type_id=16') }}" class="weui_grid">
-                <div class="weui_grid_icon">
-                    <i class="iconfont icon-dianpu"></i>
-                </div>
-                <p class="weui_grid_label">
-                    租赁权贷
-                </p>
-            </a>
-            <a href="{{ url('/article-view?type_id=17') }}" class="weui_grid">
-                <div class="weui_grid_icon">
-                    <i class="iconfont icon-gouwu"></i>
-                </div>
-                <p class="weui_grid_label">
-                    经营权贷
-                </p>
-            </a>
-            <a href="{{ url('/article-view?type_id=18') }}" class="weui_grid">
-                <div class="weui_grid_icon">
-                    <i class="iconfont icon-gouwu"></i>
-                </div>
-                <p class="weui_grid_label">
-                    产权贷
-                </p>
-            </a>
-            <a href="{{ url('/article-view?type_id=19') }}" class="weui_grid">
-                <div class="weui_grid_icon">
-                    <i class="iconfont icon-gouwu"></i>
-                </div>
-                <p class="weui_grid_label">
-                    品牌权贷
-                </p>
-            </a>
-        </div>
-
     </div>
 </div>
 
