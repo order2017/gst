c599f88cfd31fb323587cc4c2fd6522b0e8ed744
403228075 <403228075@qq.com>
2018-03-21 08:16:12 +0800

    调整类型字段

--- resources/views/include/mobile/_sudoku.blade.php
+++ resources/views/include/mobile/_sudoku.blade.php
@@ -6,42 +6,42 @@
     <div class="bd">
         <div class="weui_cells_title">共享商业</div>
         <div class="weui_grids">
-            <a href="{{ url('/article-view?type_id=5') }}" class="weui_grid" style="padding: 5px;">
+            <a href="{{ url('/article-view?type_id=3') }}" class="weui_grid" style="padding: 5px;">
                 <div class="weui_grid_icon" style="width: 100%; height: 80px; background-color: #f3cb71; text-align: center;">
                     <p style="padding-top: 30px; color: #000;">
                         商场招商
                     </p>
                 </div>
             </a>
-            <a href="{{ url('/article-view?type_id=6') }}" class="weui_grid" style="padding: 5px;">
+            <a href="{{ url('/article-view?type_id=4') }}" class="weui_grid" style="padding: 5px;">
                 <div class="weui_grid_icon" style="width: 100%; height: 80px; background-color: #98d08c; text-align: center;">
                     <p style="padding-top: 30px; color: #fff;">
                         商业招商
                     </p>
                 </div>
             </a>
-            <a href="{{ url('/article-view?type_id=7') }}" class="weui_grid" style="padding: 5px;">
+            <a href="{{ url('/article-view?type_id=5') }}" class="weui_grid" style="padding: 5px;">
                 <div class="weui_grid_icon" style="width: 100%; height: 80px; background-color: #9bc9db; text-align: center;">
                     <p style="padding-top: 30px; color: #000;">
                         商场买卖
                     </p>
                 </div>
             </a>
-            <a href="{{ url('/article-view?type_id=8') }}" class="weui_grid" style="padding: 5px;">
+            <a href="{{ url('/article-view?type_id=6') }}" class="weui_grid" style="padding: 5px;">
                 <div class="weui_grid_icon" style="width: 100%; height: 80px; background-color: #f15a24; text-align: center;">
                     <p style="padding-top: 30px; color: #fff;">
                         商业买卖
                     </p>
                 </div>
             </a>
-            <a href="{{ url('/article-view?type_id=9') }}" class="weui_grid" style="padding: 5px;">
+            <a href="{{ url('/article-view?type_id=7') }}" class="weui_grid" style="padding: 5px;">
                 <div class="weui_grid_icon" style="width: 100%; height: 80px; background-color: #fcdda5; text-align: center;">
                     <p style="padding-top: 30px; color: #000;">
                         设备买卖
                     </p>
                 </div>
             </a>
-            <a href="{{ url('/article-view?type_id=9') }}" class="weui_grid" style="padding: 5px;">
+            <a href="{{ url('/article-view?type_id=8') }}" class="weui_grid" style="padding: 5px;">
                 <div class="weui_grid_icon" style="width: 100%; height: 80px; background-color: #0071bc; text-align: center;">
                     <p style="padding-top: 30px; color: #fff;">
                         品牌拓展
@@ -52,7 +52,7 @@
 
         <div class="weui_cells_title">共享商超</div>
         <div class="weui_grids">
-            <a href="{{ url('/article-view?type_id=10') }}" class="weui_grid" style="padding: 5px;">
+            <a href="{{ url('/article-view?type_id=9') }}" class="weui_grid" style="padding: 5px;">
                 <div class="weui_grid_icon" style="width: 100%; height: 80px;">
                     <img src="{{ asset('/assets/i/rou.jpg') }}" alt="">
                 </div>
@@ -92,7 +92,7 @@
                     共享服务
                 </p>
             </a>
-            <a href="{{ url('/article-view?type_id=13') }}" class="weui_grid" style="padding: 5px;">
+            <a href="{{ url('/article-view?type_id=14') }}" class="weui_grid" style="padding: 5px;">
                 <div class="weui_grid_icon" style="width: 100%; height: 80px;">
                     <img src="{{ asset('/assets/i/gst_logo.png') }}" alt="">
                 </div>
