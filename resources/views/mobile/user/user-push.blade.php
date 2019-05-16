04c036e4937dae7e1ef995ca13c6cee02459b596
403228075@qq.com <403228075@qq.com>
2019-05-16 09:55:14 +0800

    视频上传

--- resources/views/mobile/user/user-push.blade.php
+++ resources/views/mobile/user/user-push.blade.php
@@ -182,10 +182,17 @@
     @include('vendor.ueditor.assets')
     <!-- 实例化编辑器 -->
     <script type="text/javascript">
-        var ue = UE.getEditor('contents');
+        var ue = UE.getEditor('contents', {
+            toolbars: [
+                ['simpleupload','insertimage','insertvideo']
+            ],
+            autoHeightEnabled: true,
+            autoFloatEnabled: true
+        });
         ue.ready(function() {
             ue.execCommand('serverparam', '_token', '{{ csrf_token() }}'); // 设置 CSRF token.
         });
+
     </script>
 
     <script>
